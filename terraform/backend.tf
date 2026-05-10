c60b644a709ce92fdada96175b997928dd5ed0c8
toka863 <tokakamel31@gmail.com>
2026-05-10 12:41:47 +0300

    update1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "shopflow-terraform-state-dnt"
-    key          = "stage/data-stores/mysql/terraform.tfstate"
+    key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true # Enables native S3 locking [cite: 23]
     encrypt      = true
