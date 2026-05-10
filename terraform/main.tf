b26417cdfe771afda6a30e9cc6093acbd0bce7f4
Demyana S. Zaki <Damiana21079@feng.bu.edu.eg>
2026-05-10 11:32:01 +0300

    merge dodo main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,3 @@
-<<<<<<< Updated upstream
-module "vpc" {
-  source = "./modules/vpc"
-
-  vpc_cidr           = "10.0.0.0/16"
-  vpc_name           = "main-vpc"
-
-  Public_subnet_cidr = "10.0.1.0/24"
-  private_subnet_cidr = "10.0.2.0/24"
-
-  availability_zone  = "us-east-1a"
-
-  igw_name = "main-igw"
-  nat_gw   = "main-nat-gateway"
-}
-
-
-module "ec2"{
-    source = "./modules/ec2"
-    vpc_id = module.vpc.vpc_id
-    public_subnet_id = module.vpc.public_subnet_id
-    private_subnet_id = module.vpc.private_subnet_id
-    instance_type = "t3.micro"
-    security_group = "web_sg"
-
-}
-
-
-=======
 # 1. IAM Module [cite: 191]
 module "iam" {
   source = "./modules/iam"
@@ -55,4 +26,30 @@ module "ec2" {
   iam_instance_profile = module.iam.ec2_instance_profile_name [cite: 91, 193]
   ecr_repo_url         = var.ecr_repo_url
 }
->>>>>>> Stashed changes
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr           = "10.0.0.0/16"
+  vpc_name           = "main-vpc"
+
+  Public_subnet_cidr = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
+
+  availability_zone  = "us-east-1a"
+
+  igw_name = "main-igw"
+  nat_gw   = "main-nat-gateway"
+}
+
+
+module "ec2"{
+    source = "./modules/ec2"
+    vpc_id = module.vpc.vpc_id
+    public_subnet_id = module.vpc.public_subnet_id
+    private_subnet_id = module.vpc.private_subnet_id
+    instance_type = "t3.micro"
+    security_group = "web_sg"
+
+}
+
+
