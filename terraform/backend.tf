09b7750f749b24471afc23f7d69e49cfd52770a9
Demyana S. Zaki <Damiana21079@feng.bu.edu.eg>
2026-06-10 13:17:01 +0300

    feat: setup complete multi-tier deployment configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "shopflow-terraform-state-dnt"
+    bucket       = "shopflow-terraform-state-dodo"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true # Enables native S3 locking [cite: 23]
