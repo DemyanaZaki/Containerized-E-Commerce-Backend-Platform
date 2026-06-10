3fc264c64becbbd5dc7444433f1f82d0f4e48cb4
Demyana S. Zaki <Damiana21079@feng.bu.edu.eg>
2026-06-10 19:16:56 +0300

    name-prefix

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -203,7 +203,7 @@ EOF
 ########################################
 
 resource "aws_lb_target_group" "app_tg" {
-  name     = "app-target-group"
+  name_prefix = "app-tg"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
